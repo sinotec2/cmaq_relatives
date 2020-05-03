15d2787141ca44d1216a057b6640068b64944787
Yungchuan Kuang <sinotec2@gmail.com>
2020-05-03 19:43:58 +0800

    2020/3 version
    read the mcip results for level definition

---
+++ moz2cmaqV.py
@@ -0,0 +1,40 @@
+import numpy as np
+import netCDF4
+import PseudoNetCDF as pnc
+import os,twd97,sys
+import datetime
+from pyproj import Proj
+if (len(sys.argv) != 2):
+  print ('usage: '+sys.argv[0]+' YYMM(1601)')
+yrmn=sys.argv[1]
+CRS="../mcip/sChina_27k/METCRO3D_1601.nc"
+nc = netCDF4.Dataset(CRS,'r')
+lvs_crs0= nc.VGLVLS[:]
+lvs_crs=((1013-50)*lvs_crs0+50)*-1
+nlays=nc.NLAYS
+
+#store the mozart model results
+fname='moz_41_201601.nc'
+nc = netCDF4.Dataset(fname,'r')
+v4=list(filter(lambda x:nc.variables[x].ndim==4, [i for i in nc.variables]))
+lvs= nc.VGLVLS[:]*-1
+sp_crs=np.searchsorted(lvs,lvs_crs)
+nt,nlay,nrow,ncol=(nc.variables[v4[0]].shape[i] for i in range(4))
+A5=np.zeros(shape=(nc.NVARS,nt,nlay,nrow,ncol))
+for ix in range(nc.NVARS):
+  A5[ix,:,:,:,:]=nc.variables[v4[ix]][:,:,:,:]
+
+fname='moz_41_201601.nc'
+nc = netCDF4.Dataset(fname,'r+')
+nc.VGLVLS=lvs_crs0
+for kcrs in range(nlays):
+  kmz=sp_crs[kcrs]
+  if kmz==0:
+    for ix in range(nc.NVARS):
+      nc.variables[v4[ix]][:,kcrs,:,:]=A5[ix,:,0,:,:]
+  else:
+    r=(lvs_crs[kcrs]-lvs[kmz-1])/(lvs[kmz]-lvs[kmz-1])
+    A4=A5[:,:,kmz,:,:]*r+A5[:,:,kmz-1,:,:]*(1-r)
+    for ix in range(nc.NVARS):
+      nc.variables[v4[ix]][:,kcrs,:,:]=A4[ix,:,:,:]
+nc.close()
